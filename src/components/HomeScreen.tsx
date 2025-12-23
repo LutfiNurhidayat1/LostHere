d5f271641f690e5967bb939949853cd66b841bec
LutfiNurhidayat1 <lutfi01.nurhidayat0504@gmail.com>
2025-12-24 00:52:26 +0700

    fix: correct case-sensitive import paths

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import { Search, Bell, Clock } from 'lucide-react';
-import { Button } from './ui/Button';
+import { Button } from './ui/button';
 
 interface HomeScreenProps {
   onReportLost: () => void;
